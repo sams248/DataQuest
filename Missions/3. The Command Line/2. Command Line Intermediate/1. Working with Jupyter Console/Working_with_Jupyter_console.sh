da5d4fa0b01e34aa43b5466d7be4b3a21d276ae1
Sam <40251608+sams248@users.noreply.github.com>
2018-10-16 17:28:44 -0400

    Add files via upload

---
+++ Missions/3. The Command Line/2. Command Line Intermediate/1. Working with Jupyter Console/Working_with_Jupyter_console.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+###################
+#ipython
+
+# dq = 5
+
+# dq_10 = dq * 10
+
+# dq?
+
+# help()
+
+# exit
+###################
+#ipython
+
+# %run script.py
+
+# %who
+
+# exit
+
+###################
+#ipython
+
+# %cpaste
+
+#for i in range(10):
+#    if i < 5:
+#        print(i)
+#    else:
+#       print(i * 2)
+
+# exit
+
+##################
---
+++ Missions/3. The Command Line/2. Command Line Intermediate/1. Working with Jupyter Console/script.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+	print("This is a Python script")
+
+print("Excellent!")
+s = 8
+
